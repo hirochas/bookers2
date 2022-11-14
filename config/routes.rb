d8f9dd69db007e3ae8ad11ab5e4d5b47d6c6f4c0
HiromiYoshizaki <115017300+hirochas@users.noreply.github.com>
2022-11-14 13:19:24 +0000

    [add]controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get 'books/show'
   root to: "homes#top"
   devise_for :users
+  
+   resources :users, only: [:show, :edit]
+   resources :books, only: [:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_14_070536) do
+ActiveRecord::Schema.define(version: 2022_11_14_104611) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "bo"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
