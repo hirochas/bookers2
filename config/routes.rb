47df532768141936fe3efc95fb04bc923e4827f2
HiromiYoshizaki <115017300+hirochas@users.noreply.github.com>
2022-11-15 06:46:33 +0000

    [ADD]design

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
   root to: "homes#top"
   devise_for :users
+  get "/homes/about" => "homes#about", as: "about"
   
    resources :users, only: [:show, :edit]
    resources :books, only: [:new, :index, :show]
